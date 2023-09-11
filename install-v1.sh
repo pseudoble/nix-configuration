7889d6209442e66582f00b016f822349bcf438de
Charles Josephs <cjosephs@gmail.com>
2023-09-11 13:31:56 -0400

    split install script into install and setup

--- install-v1.sh
+++ install-v1.sh
@@ -3,13 +3,6 @@
 git clone https://github.com/pseudoble/nix-configuration.git ~/.nixos
 cd ~/.nixos/tardis
 
-# Check if --generate-config passed
-if [ "$1" == "--generate-config" ]; then
-    sudo nixos-generate-config --root ./nixos
-fi
-
-sudo nixos-rebuild switch --flake .#nixos
-home-manager switch --flake .#cjosephs@nixos
-
-echo "Repoen your terminal to see changes"
+echo "Installing NixOS Configuration..."
+source ./setup-v1.sh $@
 
---
+++ setup-v1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if --generate-config passed
+if [ "$1" == "--generate-config" ]; then
+    sudo nixos-generate-config --root ./nixos
+fi
+
+sudo nixos-rebuild switch --flake .#nixos
+home-manager switch --flake .#cjosephs@nixos
+
+echo "Repoen your terminal to see changes"
+
