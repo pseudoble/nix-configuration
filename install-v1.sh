0bca09991d34a0524c1b4bd7ee67f1541eeb6b89
Charles Josephs <cjosephs@gmail.com>
2023-09-11 12:54:44 -0400

    added hardware generation to script

--- install-v1.sh
+++ install-v1.sh
@@ -3,4 +3,9 @@
 git clone https://github.com/pseudoble/nix-configuration.git ~/.nixos
 cd ~/.nixos/tardis
 
+# Check if --generate-config passed
+if [ "$1" == "--generate-config" ]; then
+    sudo nixos-generate-config --root ./nixos
+fi
+
 sudo nixos-rebuild switch --flake .#nixos
