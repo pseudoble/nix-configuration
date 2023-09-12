a2edda4b09102791005b88e64aced7ab314caf4e
Charles Josephs <cjosephs@gmail.com>
2023-09-12 10:28:15 -0400

    iterating on setup script

--- setup-v1.sh
+++ setup-v1.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-cd ~/.nixos/tardis-virtual
+FLAKE_NAME=tardis-virtual
 
-echo "Running setup with the following arguments: [$@]"
+FLAKE_PATH_BASE=$HOME/.nixos/$FLAKE_NAME
+CONFIG_PATH_BASE=$FLAKE_PATH_BASE/nixos
+NIXOS_CONFIG_PATH_BASE=/etc/nixos
+NIXOS_HARDWARE_CONFIG_PATH=$NIXOS_CONFIG_PATH_BASE/hardware-configuration.nix
+
+cd $FLAKE_PATH_BASE
+
 # Check if --generate-config passed
 if [[ "$@" == *"--copy-config"* ]]; then
-  if [ -f "/etc/nixos/configuration.nix" ]; then
-    echo "Copying /etc/nixos/configuration.nix to ./tardis-virtual"
-    cp /etc/nixos/configuration.nix ./tardis-virtual
+  if [ -f "$NIXOS_HARDWARE_CONFIG_PATH" ]; then
+    echo "Copying $NIXOS_HARDWARE_CONFIG_PATH to $CONFIG_PATH_BASE"
+    cp $NIXOS_HARDWARE_CONFIG_PATH $CONFIG_PATH_BASE
   else
-    echo "No /etc/nixos/configuration.nix found"
+    echo "No $NIXOS_HARDWARE_CONFIG_PATH found"
     exit 1
   fi
 #    sudo nixos-generate-config --root ./nixos
 fi
 
 
-sudo nixos-rebuild switch --flake .#tardis-virtual
-home-manager switch --flake .#cjosephs@tardis-virtual
+sudo nixos-rebuild switch --flake .#$FLAKE_NAME
+home-manager switch --flake .#cjosephs@$FLAKE_NAME
 
 echo "Reopen your terminal to see changes"
