a9c899afb064923d42c310c2f269b24d36476c24
Charles Josephs <cjosephs@gmail.com>
2023-11-27 07:54:25 -0500

    add arch support?

--- tardis/flake.nix
+++ tardis/flake.nix
@@ -66,9 +66,11 @@
 
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
+      homeConfigurations = forAllSystems (system:
+        let pkgs = nixpkgs.legacypackages.${system};
+        in {
         "cjosephs@nixos" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          inherit pkgs;
           extraSpecialArgs = { inherit inputs outputs; };
           modules = [
             # > Our main home-manager configuration file <
